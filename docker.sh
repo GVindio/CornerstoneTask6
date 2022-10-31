0384c4a0e22f6560214f8dc9aad8768f3f58fe42
GVindio <97354117+GVindio@users.noreply.github.com>
2022-10-31 15:24:56 -0400

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo su --
+hostname docker-server
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo docker pull nginx:latest
+sudo docker run --name mynginx1 -p 80:80 -d nginx
+
